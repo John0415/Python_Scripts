5f078ea133fe4af6897e55e046c6c25f52ed51e5
John0415 <55759107+John0415@users.noreply.github.com>
2020-01-17 18:56:23 -0600

    Update Python_File_Handling.py

--- Python_File_Handling.py
+++ Python_File_Handling.py
@@ -1,10 +1,11 @@
+# Extracting specific information from a file (  Here, it is Blast output file)
 file = open('blast-12.out', 'r')
 x = []
 for line in file:
     words = line.split()
-    x = int(words[11])
+    x = int(words[11]) # Converting 11th word to int in every line for comparision.
     if (x >=  221199) & (x <=222701):
-        y =str(x)
+        y =str(x)   # Converting integer back to string
         if y in line:
             print(line)
         else:
